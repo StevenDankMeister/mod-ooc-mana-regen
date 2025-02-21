3250a31fd3a8d29e577558c235ec6b9a12329116
StevenDankMeister <stran19@student.aau.dk>
2025-02-21 23:36:15 +0100

    Formula fix

--- src/Ooc_mana_regen.cpp
+++ src/Ooc_mana_regen.cpp
@@ -7,19 +7,21 @@ class ooc_mana_regen_announce : public PlayerScript
 {
 private:
     std::unordered_map<ObjectGuid, uint32> player_timers;
-    void UpdateMana(Player* player) {
+    void UpdateMana(Player *player)
+    {
         int max_mana = player->GetMaxPower(POWER_MANA);
         int current_mana = player->GetPower(POWER_MANA);
 
-        int mana_to_add = max_mana / sConfigMgr->GetOption<float>("OOCManaRegen.ManaRegenMult", 10);
+        int mana_to_add = max_mana * (sConfigMgr->GetOption<float>("OOCManaRegen.ManaRegenMult", 10) / 100);
         int new_mana = current_mana + mana_to_add;
 
         player->SetPower(POWER_MANA, std::min(new_mana, max_mana));
     }
-public:
-    ooc_mana_regen_announce() : PlayerScript("ooc_mana_regen") { }
 
-    void OnLogin(Player* player) override
+public:
+    ooc_mana_regen_announce() : PlayerScript("ooc_mana_regen") {}
+
+    void OnLogin(Player *player) override
     {
         if (sConfigMgr->GetOption<bool>("OOCManaRegen.Announce", true))
         {
@@ -27,14 +29,17 @@ public:
         }
     }
 
-    void OnUpdate(Player* player, uint32 diff) {
-        if (player->IsInCombat()) {
+    void OnUpdate(Player *player, uint32 diff)
+    {
+        if (player->IsInCombat())
+        {
             player_timers[player->GetGUID()] = 0;
             return;
         }
 
         player_timers[player->GetGUID()] += diff;
-        if (player_timers[player->GetGUID()] < sConfigMgr->GetOption<int>("OOCManaRegen.Timer", 5000)) return;
+        if (player_timers[player->GetGUID()] < sConfigMgr->GetOption<int>("OOCManaRegen.Timer", 5000))
+            return;
 
         UpdateMana(player);
         player_timers[player->GetGUID()] = 0;
